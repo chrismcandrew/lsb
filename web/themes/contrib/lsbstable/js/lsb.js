f012a5bdeb9e97362114f01030b4fdb1b20fbd19
Christopher McAndrew <chris@csmcreative.com>
2016-05-09 16:58:34 -0400

    Search and directions link added, looks good

--- web/themes/contrib/lsbstable/js/lsb.js
+++ web/themes/contrib/lsbstable/js/lsb.js
@@ -7,11 +7,11 @@
 		// 'use strict';
 		
 		//$("#block-lotussalon-mainnavigation").addClass('navbar-fixed-top');
-	 $(".form-text, .form-textarea, .form-search, select").addClass('form-control');
+	 $(".form-text, .form-textarea, .form-search, .form-email, select").addClass('form-control');
 		// $(".form-submit").addClass('btn btn-default');
 		// $(".cta").addClass('btn btn-secondary');
 	$("#block-lsbstable-mainnavigation ul").addClass('nav blog-nav');
-		// $("button.link").removeClass('btn-primary').addClass('label label-default');
+		 $(".button").addClass('btn btn-primary');
 		// $("#fallback-page-menu").css("display","none");
 		 $("#block-lsbstable-mainnavigation li").addClass('nav-link');
  //$("#block-lsbstable-mainnavigation li").addClass('active');
