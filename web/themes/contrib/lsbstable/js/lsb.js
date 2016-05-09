762ed1457f468045bf7411d796581baaa1a5cc22
Christopher McAndrew <chris@csmcreative.com>
2016-05-09 15:24:57 -0400

    Blog css is working and nav is functional

---
+++ web/themes/contrib/lsbstable/js/lsb.js
@@ -0,0 +1,33 @@
+(function ($) {
+
+
+
+
+	$(document).ready(function(){
+		// 'use strict';
+		
+		//$("#block-lotussalon-mainnavigation").addClass('navbar-fixed-top');
+	 $(".form-text, .form-textarea, .form-search, select").addClass('form-control');
+		// $(".form-submit").addClass('btn btn-default');
+		// $(".cta").addClass('btn btn-secondary');
+	$("#block-lsbstable-mainnavigation ul").addClass('nav blog-nav');
+		// $("button.link").removeClass('btn-primary').addClass('label label-default');
+		// $("#fallback-page-menu").css("display","none");
+		 $("#block-lsbstable-mainnavigation li").addClass('nav-link');
+ //$("#block-lsbstable-mainnavigation li").addClass('active');
+	});
+})(jQuery);
+
+
+
+// (function ($) {  
+
+//   Drupal.behaviors.lotussalon = {
+
+//     attach: function (context, settings) {            
+
+   
+
+//   }};
+
+// })(jQuery);
