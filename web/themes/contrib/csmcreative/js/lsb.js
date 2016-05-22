55f20eb5b4dd4b4fce767cca551d97bebf233882
Christopher McAndrew <chris@csmcreative.com>
2016-05-21 22:35:29 -0400

    Add csmcreative theme

---
+++ web/themes/contrib/csmcreative/js/lsb.js
@@ -0,0 +1,35 @@
+(function ($) {
+
+
+
+
+$(document).ready(function(){
+// 'use strict';
+
+	//$("#block-lotussalon-mainnavigation").addClass('navbar-fixed-top');
+	$(".form-text, .form-textarea, .form-search, .form-email, select").addClass('form-control');
+	// $(".form-submit").addClass('btn btn-default');
+	// $("img").addClass('img-fluid');
+	$("#block-lsbstable-mainnavigation ul").addClass('nav blog-nav');
+	$(".button").addClass('btn btn-primary');
+	// $("#fallback-page-menu").css("display","none");
+	$("#block-lsbstable-mainnavigation li").addClass('nav-link');
+	$("ul.price-list").addClass('list-group');
+	$("ul.price-list li").addClass('list-group-item');
+	$("ul.price-list span").addClass('price strong');
+});
+})(jQuery);
+
+
+
+// (function ($) {  
+
+//   Drupal.behaviors.lotussalon = {
+
+//     attach: function (context, settings) {            
+
+
+
+//   }};
+
+// })(jQuery);
---
+++ web/themes/contrib/csmcreative/js/lsb.js.bak.js
@@ -0,0 +1,20 @@
+(function ($) {
+
+
+
+
+$(document).ready(function(){
+// 'use strict';
+
+
+	//$("#block-lotussalon-mainnavigation").addClass('navbar-fixed-top');
+	$(".form-text, .form-textarea, .form-search, .form-email, select").addClass('form-control');
+	// $(".form-submit").addClass('btn btn-default');
+	// $("img").addClass('img-fluid');
+	$("#block-lsbstable-mainnavigation ul").addClass('nav blog-nav');
+	$(".button").addClass('btn btn-primary');
+	// $("#fallback-page-menu").css("display","none");
+	$("#block-lsbstable-mainnavigation li").addClass('nav-link');
+	//$("#block-lsbstable-breadcrumbs nav ol").addClass('breadcrumb');
+});
+})(jQuery);
